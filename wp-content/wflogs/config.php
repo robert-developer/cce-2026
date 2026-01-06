e88085612ded769f7c47aff544fb6f21c94dfad2
Robert Huamani <robert.huamani107@gmail.com>
2026-01-06 01:05:20 -0500

    adding last commit of the project

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1761165262;s:7:"authKey";s:64:"I,41[z3ry$K_l $-lq-&:QA5v`Z$mA4/vUS/LV]K:$X*QDlqV>ML}2NWE3 TIcd*";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:1;s:13:"attackDataKey";i:3813;}
